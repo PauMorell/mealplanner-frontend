50c351310ce3151ca12211d51344d0fe4a131e9b
PauMorell <paumorell3@gmail.com>
2024-06-20 11:38:53 +0200

    He trobat sa manera d'importar data :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import { Button } from "./components/ui/button"
+import useFetch from "./hooks/usefetch"
 
 function App() {
+  const {loading, error, data} = useFetch("http://localhost:1337/api/categorias")
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error :( </p>
+
+    /* const categories = data?.data || [] */
+
+   
+  /* console.log(data.data) */
+  let categors = data.data;
+  console.log(categors)
+  categors.map(cat => {
+    console.log(cat.attributes.Nom)
+  })
+
   return (
     <>
       <Button>De lokos</Button>
+      <div>
+      <div>
+        {/* {categories.map((item) => (
+          <p key={item.id}>{item.attributes.Nom}</p>
+        ))} */}
+      </div>
+      </div>
     </>
   )
 }
