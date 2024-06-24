af021a6c175e9d373d598037fd685e8324d092f0
PauMorell <paumorell3@gmail.com>
2024-06-24 09:20:33 +0200

    Login popup funcional

--- src/AdminCard.jsx
+++ src/AdminCard.jsx
@@ -1,27 +1,32 @@
 import { useEffect, useState } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle,CardFooter } from "@/components/ui/card";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
-export default function AdminCard() {
+export default function AdminCard({ token, onLogout }) {
   const [categorias, setCategorias] = useState([]);
   const [selectedCategoria, setSelectedCategoria] = useState("");
   const [platNou, setPlatNou] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
-      const categoriasResponse = await fetch('http://localhost:1337/api/categorias?populate[plats][fields][0]=id&populate[plats][fields][1]=Nom&fields[0]=id&fields[1]=Nom');
+      const categoriasResponse = await fetch('http://localhost:1337/api/categorias?populate[plats][fields][0]=id&populate[plats][fields][1]=Nom&fields[0]=id&fields[1]=Nom', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
       const categoriasData = await categoriasResponse.json();
       setCategorias(categoriasData.data);
     };
 
-    fetchData();
-  }, []);
+    if (token) {
+      fetchData();
+    }
+  }, [token]);
 
   const handleCategoriaChange = (value) => {
     setSelectedCategoria(value);
-    console.log(selectedCategoria)
   };
 
   const handlePlatChange = (e) => {
@@ -35,7 +40,8 @@ export default function AdminCard() {
         const response = await fetch('http://localhost:1337/api/plats', {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
           },
           body: JSON.stringify({
             data: {
@@ -45,7 +51,11 @@ export default function AdminCard() {
           })
         });
         if (response.ok) {
-          const categoriasResponse = await fetch('http://localhost:1337/api/categorias?populate[plats][fields][0]=id&populate[plats][fields][1]=Nom&fields[0]=id&fields[1]=Nom');
+          const categoriasResponse = await fetch('http://localhost:1337/api/categorias?populate[plats][fields][0]=id&populate[plats][fields][1]=Nom&fields[0]=id&fields[1]=Nom', {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          });
           const categoriasData = await categoriasResponse.json();
           setCategorias(categoriasData.data);
 
@@ -67,31 +77,41 @@ export default function AdminCard() {
           <CardTitle>Afegir un plat nou</CardTitle>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleAddPlat}>
-            <div>
-              <Select className="justify-center" onValueChange={handleCategoriaChange} value={selectedCategoria}>
-                <SelectTrigger className="w-[140px] text-sm">
-                  <SelectValue placeholder="Categoria" />
-                </SelectTrigger>
-                <SelectContent>
-                  {categorias.map(cat => (
-                    <SelectItem key={cat.id} value={cat.id}>{cat.attributes.Nom}</SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
+          {token ? (
+            <form onSubmit={handleAddPlat}>
+              <div>
+                <Select className="justify-center" onValueChange={handleCategoriaChange} value={selectedCategoria}>
+                  <SelectTrigger className="w-[140px] text-sm">
+                    <SelectValue placeholder="Categoria" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {categorias.map(cat => (
+                      <SelectItem key={cat.id} value={cat.id}>{cat.attributes.Nom}</SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
 
-            <div className="flex">
-              <Input 
-                className="my-2" 
-                placeholder="Plat nou"
-                value={platNou}
-                onChange={handlePlatChange}
-              />
-              <Button className="mx-2 my-2 h-[48px] ml-8" variant="destructive" type="submit">Afegir</Button>
-            </div>
-          </form>
+              <div className="flex">
+                <Input 
+                  className="my-2" 
+                  placeholder="Plat nou"
+                  value={platNou}
+                  onChange={handlePlatChange}
+                />
+                <Button className="mx-2 my-2 h-[48px] ml-8" variant="destructive" type="submit">Afegir</Button>
+              </div>
+              <aside className="text-xs" > <em>*Perquè aparegui el plat nou, heu de recarregar la pàgina</em></aside>
+            </form>
+          ) : (
+            <div className="text-red-500">Per afegir un plat nou, primer heu de iniciar sessió.</div>
+          )}
         </CardContent>
+        {token && (
+          <CardFooter>
+            <Button variant="secondary" onClick={onLogout}>Logout</Button>
+          </CardFooter>
+        )}
       </Card>
     </div>
   );
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DayCard from './DayCard';
 import ResumenTable from './ResumenTable';
 import AdminCard from './AdminCard';
 import Login from './Login';
+import { Button } from './components/ui/button';
+import Modal from 'react-modal';
 
 const weekDays = ["Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte", "Diumenge"];
 
@@ -10,9 +12,22 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [token, setToken] = useState('');
 
+  useEffect(() => {
+    const savedToken = localStorage.getItem('token');
+    if (savedToken) {
+      setToken(savedToken);
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   const handleLogin = (jwt) => {
-    setIsAuthenticated(true);
     setToken(jwt);
+  };
+
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setToken(null);
   };
 
 
@@ -29,19 +44,45 @@ function App() {
         ));
     }
 
+    const [visibleLogin, setVisibleLogin] = useState(false);
+
+    function closeModal() {
+      setVisibleLogin(false);
+    }
+
     return (
       <>
-      <div className='p-10'>
+{/*       <div className='p-10'>
       {isAuthenticated ? (
-        <div>Benvingut, has iniciat sessió correctament</div> 
+            <div className='flex justify-between'>
+          <h4>Benvingut, has iniciat sessió correctament</h4>
+          <Button onClick={handleLogout}>Finalitza la sessió</Button>
+          </div> 
       ) : (
         <Login onLogin={handleLogin} />
       )}
-    </div>
+    </div> */}
+                
         <div className="div-app">
+          <Button onClick={() => setVisibleLogin(true)}>Login button</Button>
           <div className='flex-layout-container'>
             <div className='grid-layout-container'>
-              <AdminCard></AdminCard>
+            <div>
+            <AdminCard token={token} onLogout={handleLogout} />
+
+            <div>
+              <Modal 
+              isOpen={visibleLogin}
+              onRequestClose={closeModal}
+              
+              >
+                <Login onLogin={handleLogin && closeModal} />
+                <Button onClick={closeModal}></Button>
+                </Modal>
+            </div>
+            
+            {/* {!token && <Login onLogin={handleLogin} />} */}
+          </div>
                 {weekDays.map((dia, index) => (
                     <DayCard className="grid-layout-item" key={index} dia={dia} updateDia={updateDia} />
                 ))}
